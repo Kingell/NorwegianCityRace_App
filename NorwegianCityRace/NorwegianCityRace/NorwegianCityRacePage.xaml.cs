2a71f7a8101ecf2d85c0011a6eafaa77bc3bbc3a
aHHa47 <ahmed-ak2@live.no>
2018-02-22 17:59:33 +0100

    New modified files

---
+++ NorwegianCityRace/NorwegianCityRace/NorwegianCityRacePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace NorwegianCityRace
+{
+    public partial class NorwegianCityRacePage : ContentPage
+    {
+        public NorwegianCityRacePage()
+        {
+            InitializeComponent();
+        }
+
+
+
+    }
+}
