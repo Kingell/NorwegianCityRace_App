e5a91d8a58f41951af8b71138340b8ae3933055a
aHHa47 <ahmed-ak2@live.no>
2018-02-23 09:53:44 +0100

    xam.media.plugin added

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -11,15 +11,16 @@ using Android.OS;
 namespace NorwegianCityRace.Droid
 {
     [Activity(Label = "NorwegianCityRace.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+          //  TabLayoutResource = Resource.Layout.Tabbar;
+          //  ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
 
+           // Plugin.CurrentActivity.CrossCurrentActivity.Current.Activity = this;
             global::Xamarin.Forms.Forms.Init(this, bundle);
 
             LoadApplication(new App());
