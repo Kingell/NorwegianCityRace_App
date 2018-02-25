6ac434cf94651d991ae6eba625d4ef83cf2a6650
aHHa47 <ahmed-ak2@live.no>
2018-02-25 19:32:54 +0100

    modified some files

---
+++ Models/Group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Models
+{
+    public class Group
+    {
+
+        public string Name { get; set; }
+        public string Picture { get; set; }
+
+
+
+        public Group()
+        {
+           
+        }
+    }
+}
